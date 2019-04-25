04369d96048a90ad902bc9df6017e8c6242b67ad
Bianca <bianca_jacob2@hotmail.com>
2019-04-25 10:30:31 -0300

    Calculadora

---
+++ file.py
@@ -0,0 +1,68 @@
+# Class Veiculo 
+
+# cor 
+# rodas 
+# motor 
+# porta 
+# assento(banco)
+
+# caminhão 
+# ferrari 
+
+# caminhão.cor=bege 
+# ferrari.cor=vermelha 
+
+# roda 4 rodas 
+
+# ferrari porta - vidro eletrico 
+# caminhão porta - vidro manual 
+
+# fiesta
+# 4 portas 
+
+#----------------------------------------------------------
+# class Pessoa: 
+#     def __init__(self, nome):
+#         self.nome=nome
+    
+#     def __str__(self): 
+#         return self.nome 
+# ninja= Pessoa('Raianey')
+# print(ninja)
+#-------------------------------------------------------------
+#arquivo animal_estimacao.py
+# class AnimalEstimacao(): 
+#     def __init__(self, nome, especie, dono):
+#         self.nome=nome
+#         self.especie=especie
+#         self.dono= dono 
+#     def __str__(self)
+#         return self.nome
+# #novo arquivo
+# import anima_estimacao as animal 
+# isabellita= animal.AnimalEstimacao('Isabellita', 'capivara','taina')
+# dono1 =AnimalEstimacao('Taina')
+# capivara=AnimalEstimacao('capivara de santa isabel')
+# isabellyta = AnimalEstimacao('Isinha')
+#------------------------------------------------------------------
+# class Calculadora: 
+#     def __init__(self,a,b):
+#         self.a = a 
+#         self.b = b 
+#     def soma(self): 
+#         return self.a + self.b 
+    
+#     def subtrai(self): 
+#         return self.a - self.b 
+    
+#     def multiplica(self): 
+#         return self.a * self.b 
+    
+#     def dividi(self): 
+#         return self.a / self.b 
+
+# teste= Calculadora(128,2) 
+# print('Soma:', teste.multiplica()
+
+# )
+#-------------------------------------------------------------
